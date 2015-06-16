795ac4994128eca8c2cdc0e44c3992db00962c5e
mirnhoj <mirnhoj@gmail.com>
2015-06-16 14:49:58 -0600

    Update soompi.user.js

--- soompi.user.js
+++ soompi.user.js
@@ -9,13 +9,15 @@
 $(document).ready(function () {
 	songs = document.getElementsByClassName('title-container');
 
-	songs.addEventListener("mouseover", function( event ) {   
-	  // highlight the mouseover target
-	  event.target.style.color = "orange";
+	for (var i = 0; i < songs.length; i++){
+		songs[i].addEventListener("mouseover", function( event ) {   
+		  // highlight the mouseover target
+		  event.target.style.color = "orange";
 
-	  // reset the color after a short delay
-	  setTimeout(function() {
-		event.target.style.color = "";
-	  }, 500);
-	}, false);
+		  // reset the color after a short delay
+		  setTimeout(function() {
+			event.target.style.color = "";
+		  }, 500);
+		}, false);
+	}
 });
